ebac2fc11f403e12d9c3b4e37dfcb0e70a098873
Melody Lee <melody.lee357@gmail.com>
2022-06-26 13:24:09 -0400

    alter song4u function to return spotify link baed on age

--- song4u/index.js
+++ song4u/index.js
@@ -39,11 +39,28 @@ module.exports = async function (context, req) {
         id = "Unknown"
     }
 
-    context.log(id);
+    // context.log(id);
+
+    // each generation corresponds with a unique spotify url
+    const songs = {
+        "GenZ":"https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf", 
+
+        "GenY":"https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9", 
+
+        "GenX":"https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d", 
+
+        "BabyBoomers":"https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50", 
+
+        "Unknown":"https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"}
+
+    const val = songs[id]
+
+    // the message that will be returned to the message sender
+    const message = `We guessed you're part of this generation: ${id}! Happy listening! ${val}`;
 
     context.res = {
         // first item of media that is texted
-        body: id
+        body: message
     };
 };
 
