54485a7b73dfd6a5b599c08cf6f0ac60868e71e7
Melody Lee <melody.lee357@gmail.com>
2022-07-03 15:04:43 -0400

    correct queryObject.body to queryObject.Body

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -2,12 +2,11 @@ const qs = require('qs');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
+    context.log(req.body);
     const queryObject = qs.parse(req.body);
-    context.log(queryObject.body);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: queryObject.body
+        body: queryObject.Body
     };
 }
