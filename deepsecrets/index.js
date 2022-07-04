19e5d6b7f1d7f1be2e1f32be5958bf77790ed344
Melody Lee <melody.lee357@gmail.com>
2022-07-03 21:03:54 -0400

    change deepsecrets to return a random secret

--- deepsecrets/index.js
+++ deepsecrets/index.js
@@ -55,7 +55,7 @@ async function createDocument(newItem) {
 
     // query to return top items, organized by time stamp column (c._ts)
     const querySpec = {
-    query: "SELECT top 1 * FROM c order by c._ts desc"
+    query: "SELECT * FROM c"
     };
 
     // read all items in the Items container
@@ -81,9 +81,10 @@ module.exports = async function (context, req) {
     let message = queryObject.Body; // user's input
     let document = {"message": message} // now in json form
     let items = await createDocument(document);
+    let random_value = Math.floor(items.length * Math.random());
 
     // formats text output for your user
-    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[0].message)}`
+    const responseMessage = `Thanks 😊! Stored your secret "${message}". 😯 Someone confessed that: ${JSON.stringify(items[random_value].message)}`
 
     context.res = {
         // status: 200, /* Defaults to 200 */
