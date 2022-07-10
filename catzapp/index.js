949e844ba15e968ad9d523c80b7518e844d611e7
Melody Lee <melody.lee357@gmail.com>
2022-07-10 14:54:34 -0400

    change to return 4 cats

--- catzapp/index.js
+++ catzapp/index.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("button1");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", async function() {
 
     const name1 = document.getElementById("name1").value;
     const name2 = document.getElementById("name2").value;
@@ -12,8 +12,15 @@ button.addEventListener("click", function() {
 
     const fetch_url = `${AZURE_URL}&name1=${name1}&name2=${name2}&name3=${name3}&name4=${name4}`
 
-    document.getElementById("image").src = "https://cataas.com/cat/says/" + cat;
-    
-    
+    const resp = await fetch(fetch_url, {
+        method: "GET"
+    });
+
+    const data = await resp.json(); // change what is received into a json object
+
+    document.getElementById("image1").src = "data:image/png;base64," + data.cat1;
+    document.getElementById("image2").src = "data:image/png;base64," + data.cat2;
+    document.getElementById("image3").src = "data:image/png;base64," + data.cat3;
+    document.getElementById("image4").src = "data:image/png;base64," + data.cat4;
     
 });
