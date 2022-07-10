4b785d7190efcde8a23ba0b6bb550a92ee710b8a
Melody Lee <melody.lee357@gmail.com>
2022-07-09 21:41:00 -0400

    functioning code for bunnimage

--- bunnimage/index.js
+++ bunnimage/index.js
@@ -1,37 +1,79 @@
-const bunnForm = document.getElementById('bunnForm');
+const bunnForm = document.getElementById("bunnForm");
 
-// every time someone submits the form, get value
-bunnForm.addEventListener('submit', async function (event) {
-    event.preventDefault()
-    const username = document.getElementById("username").value
-    const output = document.getElementById("output")
-    if (username == "") {
-        alert("No name error.")
-        return;
-    }
-
-    // get the file input element
-    let fileInput = document.getElementById("image");
-    // get the file
-    const file = fileInput.files[0];
+bunnForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
     var payload = new FormData(bunnForm);
-    payload.append("file", file)
 
-    console.log(payload);
+    var username = document.getElementById("username").ariaValueMax;
+    const output = document.getElementById("output");
 
-
-    // can contain information, will eventually be sent to endpoint
-    var payload = new FormData(bunnForm);
-    const endpoint = "https://melodybitproj.azurewebsites.net/api/bunnimage-upload?code=G0ZBcuVH4YW1EOIEGzSwGa03adJtU7upK-xIZ7tsLu86AzFu1YzdwQ==";
-    const options = {
-        "method":"POST",
-        "body": payload,
-        headers: {
-            "codename": username,
-            "Content-Type": "multipart/form-data"
-        }
+    if (username!= "") {
+        output.textContent = "Thanks!"
+        const resp = await fetch("https://melodybitproj.azurewebsites.net/api/bunnimage-upload?code=G0ZBcuVH4YW1EOIEGzSwGa03adJtU7upK-xIZ7tsLu86AzFu1YzdwQ==",
+        {
+            method: "POST",
+            headers: {
+                codename: username,
+            },
+            body: payload,
+        })
+        var data = await resp.text();
+    output.textContent = "Your image has been stored successfully!";
+    } else {
+        alert("No name error.");
     }
-    const resp = await fetch(endpoint, options);
-    const data = await resp.text();
-    output.textContent = "Your image has been stored successfully!"
 });
+
+const downloadButton = document.getElementById("button1");
+
+downloadButton.addEventListener("click", async function (event) {
+    var username = document.getElementById("downloadusername").ariaValueMax;
+    console.log("attempting to get your image...")
+
+    const url = ""
+
+    const resp = await fetch(url, {
+        method: "GET",
+        headers: {
+            username: username,
+        }
+    })
+})
+
+// NOTE: THE FOLLOWING LINES OF CODE DID NOT WORK, THIS IS PULLED FROM THE WEEK 4 PART 3 VIDDEO at 1:11 
+// const bunnForm = document.getElementById('bunnForm');
+
+// // every time someone submits the form, get value
+// bunnForm.addEventListener('submit', async function (event) {
+//     // event.preventDefault()
+//     const username = document.getElementById("username").value
+//     const output = document.getElementById("output")
+//     if (username == "") {
+//         alert("No name error.")
+//         return;
+//     }
+
+//     // get the file input element
+//     let fileInput = document.getElementById("image");
+//     // get the file
+//     const file = fileInput.files[0];
+//     var payload = new FormData(bunnForm);
+//     payload.append("file", file)
+
+//     console.log(payload);
+
+
+//     // can contain information, will eventually be sent to endpoint
+//     const endpoint = "https://melodybitproj.azurewebsites.net/api/bunnimage-upload?code=G0ZBcuVH4YW1EOIEGzSwGa03adJtU7upK-xIZ7tsLu86AzFu1YzdwQ==";
+//     const options = {
+//         "method":"POST",
+//         "body": payload,
+//         headers: {
+//             "codename": username,
+//             "Content-Type": "multipart/form-data"
+//         }
+//     }
+//     const resp = await fetch(endpoint, options);
+//     const data = await resp.text();
+//     output.textContent = "Your image has been stored successfully!"
+// });
