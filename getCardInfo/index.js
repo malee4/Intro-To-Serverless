76022ddc4357b4ebdcde751beb422e4eb45d31b0
Melody Lee <melody.lee357@gmail.com>
2022-07-21 03:49:01 -0400

    ifnish draft of upload to cosmosDB function

--- getCardInfo/index.js
+++ getCardInfo/index.js
@@ -1,14 +1,11 @@
+// for reading information from the card
 const { v4: uuidv4 } = require('uuid');
 const sleep = require('util').promisify(setTimeout);
 const { FormRecognizerClient, AzureKeyCredential } = require("@azure/ai-form-recognizer");
 
-const config = {
-    endpoint: process.env.COSMOS_ENDPOINT,
-    key: process.env.COSMOS_KEY,
-    databaseId: "BusinessCardInfoStorer",
-    containerId: "cards",
-    partitionKey: {kind: "Hash", paths: ["/cards"]}
-  };
+// for uploading to CosmosDB
+const qs = require('qs');
+const CosmosClient = require("@azure/cosmos").CosmosClient;
 
 module.exports = async function (context, myBlob) {
 
@@ -36,13 +33,58 @@ module.exports = async function (context, myBlob) {
 
         // IF TIME: reorganize info from businessCard
 
-        // upload to CosmosDB
-
-
+        // upload to CosmosDB (TO DO: add image file)
+        await uploadJSON(businessCard, context.bindingData.uri);
 
     } catch (err) {
         context.log(err);
         return;
     }
-
 };
+
+// configuration for Cosmos DB upload
+const config = {
+    endpoint: process.env.COSMOS_ENDPOINT,
+    key: process.env.COSMOS_KEY,
+    databaseId: "BusinessCardInfoStorer",
+    containerId: "cards",
+    partitionKey: {kind: "Hash", paths: ["/cards"]}
+  };
+
+async function uploadJSON(businessCard, URL) {
+    // append the url to the businessCard json
+    businessCard['CARD_URL'] = URL;
+    const { endpoint, key, databaseId, containerId } = config;
+    const client = new CosmosClient({ endpoint, key });
+    const database = client.database(databaseId);
+    const container = database.container(containerId);
+
+    await create(client, databaseId, containerId);
+
+    // add the businessCard to the cosmos DB container
+    const { resource: businessCard } = await container.items.create(businessCard);
+    console.log(`\r\nUploaded new card: ${businessCard.id} - ${businessCard.description}\r\n`);
+
+    return "finished upload"
+}
+
+// This script ensures that the database is setup and populated correctly
+async function create(client, databaseId, containerId) {
+    const partitionKey = config.partitionKey;
+  
+    // create the database if it does not exist
+    const { database } = await client.databases.createIfNotExists({
+      id: databaseId
+    });
+    console.log(`Created database:\n${database.id}\n`);
+  
+    // create the container if it does not exist
+    const { container } = await client
+      .database(databaseId)
+      .containers.createIfNotExists(
+        { id: containerId, partitionKey },
+        { offerThroughput: 400 }
+      );
+  
+    console.log(`Created container:\n${container.id}\n`);
+}
