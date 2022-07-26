26e089bcdb7d70f6f7f2345098a3e1ad1873a0cc
Melody Lee <melody.lee357@gmail.com>
2022-07-26 14:05:04 -0400

    create async translate function

--- cardTranslator/index.js
+++ cardTranslator/index.js
@@ -1,19 +1,41 @@
+// get the packages
 const axios = require('axios').default;
 const { v4: uuidv4 } = require('uuid');
-
-let endpoint = process.env.TRANSLATOR_ENDPOINT
-let key = process.env.TRANSLATOR_KEY
+const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+}
+
+async function translate(text, toLanguage) {
+    let endpoint = process.env.TRANSLATOR_ENDPOINT;
+    let key = process.env.TRANSLATOR_KEY;
+    let location = "East US";
+
+    const resp = await fetch(endpoint + 'translate', {
+        method: 'POST',
+        headers: {
+            'Ocp-Apim-Subscription-Key': key,
+            'Ocp-Apim-Subscription-Region': location,
+            'Content-type': 'application/json'
+        },
+        params: {
+            'api-version': '3.0',
+            'to': [toLanguage]
+        },
+        data: [text],
+        responseType: 'json'
+    });
+
+    let data = await resp.json();
+
+    return data
+
 }
